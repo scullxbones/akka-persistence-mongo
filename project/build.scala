053d142e932f1cb7e6e32f1cf38b270ec5b79bad
Brian Scully <scullduggery@gmail.com>
2014-09-07 15:43:00 -0400

    Release v0.1.2

--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ import SonatypeKeys._
 
 object AppBuilder extends Build {
   
-  val VERSION = "0.1.2-SNAPSHOT"
+  val VERSION = "0.1.2"
   val SCALA_VERSION = "2.10.0"
   val ORG = "com.github.scullxbones"
   val POM_XTRA = {
