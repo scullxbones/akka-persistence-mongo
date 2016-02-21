92269cd0c6f6e717ff8b70535ee961dc3eb1f9b5
Brian Scully <scullduggery@gmail.com>
2016-02-20 20:54:40 -0500

    Fix implicit

--- common/src/test/scala/akka/contrib/persistence/mongodb/JournalSerializableSpec.scala
+++ common/src/test/scala/akka/contrib/persistence/mongodb/JournalSerializableSpec.scala
@@ -70,11 +70,11 @@ abstract class JournalSerializableSpec(extensionClass: Class[_], database: Strin
     |  class = "akka.contrib.persistence.mongodb.MongoSnapshots"
     }""".stripMargin)
 
-  implicit val defaultPatience =
-    PatienceConfig(timeout = 5.seconds.dilated, interval = 500.millis.dilated)
-
   "A journal" should "support writing serializable events" in withConfig(config(extensionClass), "akka-contrib-mongodb-persistence-journal") { case (as,_) =>
     implicit val system = as
+    implicit val defaultPatience =
+      PatienceConfig(timeout = 5.seconds.dilated, interval = 500.millis.dilated)
+
     val pa = as.actorOf(OrderIdActor.props)
     pa ! Increment
     pa ! Increment
@@ -87,6 +87,9 @@ abstract class JournalSerializableSpec(extensionClass: Class[_], database: Strin
 
   it should "support restoring serializable events" in withConfig(config(extensionClass), "akka-contrib-mongodb-persistence-journal") { case (as,_) =>
     implicit val system = as
+    implicit val defaultPatience =
+      PatienceConfig(timeout = 5.seconds.dilated, interval = 500.millis.dilated)
+
     val pa = as.actorOf(OrderIdActor.props)
     whenReady((pa ? Get)(5.second.dilated)) {
       _ shouldBe 5
