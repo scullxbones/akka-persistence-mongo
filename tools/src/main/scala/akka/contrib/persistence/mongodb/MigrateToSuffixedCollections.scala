df48e00a481bf20ba9dbb6cdc9a6e9119b6bca54
JeanFrancoisGuena <jeanfrancois.guena@orange.com>
2016-09-26 16:20:43 +0200

    Refactor migration tool code

--- tools/src/main/scala/akka/contrib/persistence/mongodb/MigrateToSuffixedCollections.scala
+++ tools/src/main/scala/akka/contrib/persistence/mongodb/MigrateToSuffixedCollections.scala
@@ -9,13 +9,14 @@ import akka.actor.ActorSystem
 import com.mongodb.casbah.Imports._
 import com.typesafe.config.Config
 
+import akka.contrib.persistence.mongodb.JournallingFieldNames._
+
 import scala.util.Try
+import scala.util.Random
 
 class MigrateToSuffixedCollections(system: ActorSystem, config: Config) extends CasbahMongoDriver(system, config) {
 
   def migrateToSuffixCollections(): Unit = {
-    import akka.contrib.persistence.mongodb.JournallingFieldNames._
-    import scala.util.Random
 
     // INIT //
 
@@ -31,161 +32,15 @@ class MigrateToSuffixedCollections(system: ActorSystem, config: Config) extends 
       return
     }
 
-    // create a temporary collection
-    val temporaryCollectionName = s"migration2suffix-${System.currentTimeMillis()}-${Random.nextInt(1000)}"
-    val temporaryCollection = collection(temporaryCollectionName)
+    // JOURNALS // 
 
-    def pidQuery(persistenceId: String) = (PROCESSOR_ID $eq persistenceId)
+    handleMigration(journal) //, settings.JournalCollection, journal, getJournalCollectionName, journalWriteConcern, "journals")
 
-    // JOURNALS //
+    // SNAPSHOTS //  
 
-    // aggregate persistenceIds, contained in unique journal, and store them in temporary collection
-    journal.aggregate(
-      MongoDBObject("$group" -> MongoDBObject("_id" -> s"$$$PROCESSOR_ID")) ::
-        MongoDBObject("$out" -> temporaryCollectionName) ::
-        Nil).results
+    handleMigration(snaps) //, settings.SnapsCollection, snaps, getSnapsCollectionName, snapsWriteConcern, "snapshots")
 
-    // for each retrieved persistenceId, insert corresponding records from unique journal
-    // to appropriate (and newly created) suffixed journal, then remove them from unique journal
-    if (temporaryCollection.count() > 0) {
-      val totalCount = journal.count()
-      temporaryCollection.find().toSeq.groupBy(tempDbObject => getJournalCollectionName(tempDbObject.get("_id").toString)).foldLeft(0L, 0L) {
-        case ((done, ignored), (collectionName, tempDbObjects)) => {
-          val newCollection = journal(tempDbObjects.head.get("_id").toString)
-          if (!collectionName.equals(settings.JournalCollection)) {
-            tempDbObjects.foldLeft(0L, 0L) {
-              case ((ok, tot), tdbo) =>
-                val query = pidQuery(tdbo.get("_id").toString)
-                val cnt = journal.count(query).toLong
-                journal.find(query).foldLeft(0L) {
-                  case (i, dbo) =>
-                    Try { newCollection.insert(dbo, journalWriteConcern) } map { _ =>
-                      i + 1
-                    } recover {
-                      case _: Throwable =>
-                        logger.warn(s"Errors occurred when trying to insert record in '$collectionName' journal")
-                        i
-                    } getOrElse (i)
-                } match { case ssTotOk => (ok + ssTotOk, tot + cnt) }
-            } match {
-              case (inserted, count) =>
-                logger.info(s"$inserted/$count records were inserted into '$collectionName' journal")
-                tempDbObjects.foldLeft(0L) {
-                  case (ok, tdbo) =>
-                    val query = pidQuery(tdbo.get("_id").toString)
-                    Try { journal.remove(query, journalWriteConcern) } map { r =>
-                      ok + r.getN
-                    } recover {
-                      case _: Throwable =>
-                        logger.warn(s"Errors occurred when trying to remove records, previously copied to '$collectionName' journal, from '${settings.JournalCollection}' journal")
-                        ok
-                    } getOrElse (ok)
-                } match {
-                  case removed =>
-                    logger.info(s"$removed/$count records, previously copied to '$collectionName' journal, were removed from '${settings.JournalCollection}' journal")
-                    if (removed < inserted) (done + removed, ignored) else (done + inserted, ignored)
-                }
-            }
-          } else {
-            tempDbObjects.foldLeft(0L) {
-              case (tot, tdbo) =>
-                val query = pidQuery(tdbo.get("_id").toString)
-                val cnt = journal.count(query).toLong
-                tot + cnt
-            } match { case ssTotIgnored => 
-                logger.info(s"$ssTotIgnored records were ignored and remain in '${settings.JournalCollection}' journal")
-                (done, ignored + ssTotIgnored)
-            }
-          }
-        }
-      } match { 
-        case (totalOk, totalIgnored) => 
-          logger.info(s"JOURNALS: $totalOk/$totalCount records were successfully transfered to suffixed collections")
-          if ( totalIgnored > 0) {
-            logger.info(s"JOURNALS: $totalIgnored/$totalCount records were ignored and remain in '${settings.JournalCollection}' journal")
-            if (totalIgnored + totalOk == totalCount)
-              logger.info(s"JOURNALS: $totalOk + $totalIgnored = $totalCount, all records were successfully handled")
-            else
-              logger.warn(s"JOURNALS: $totalOk + $totalIgnored does NOT equal $totalCount, check remaining records  in '${settings.JournalCollection}' journal")
-          }
-      }
-    }
-
-    // SNAPSHOTS //
-
-    // aggregate persistenceIds, contained in unique snaps, and store them in temporary collection
-    snaps.aggregate(
-      MongoDBObject("$group" -> MongoDBObject("_id" -> s"$$$PROCESSOR_ID")) ::
-        MongoDBObject("$out" -> temporaryCollectionName) ::
-        Nil).results
-
-    // for each retrieved persistenceId, insert corresponding records from unique snaps
-    // to appropriate (and newly created) suffixed snaps, then remove them from unique snaps
-    if (temporaryCollection.count() > 0) {
-      val totalCount = snaps.count()
-      temporaryCollection.find().toSeq.groupBy(tempDbObject => getSnapsCollectionName(tempDbObject.get("_id").toString)).foldLeft(0L, 0L) {
-        case ((done, ignored), (collectionName, tempDbObjects)) => {
-          if (!collectionName.equals(settings.SnapsCollection)) {
-            val newCollection = snaps(tempDbObjects.head.get("_id").toString)
-            tempDbObjects.foldLeft(0L, 0L) {
-              case ((ok, tot), tdbo) =>
-                val query = pidQuery(tdbo.get("_id").toString)
-                val cnt = snaps.count(query).toLong
-                snaps.find(query).foldLeft(0L) {
-                  case (i, dbo) =>
-                    Try { newCollection.insert(dbo, snapsWriteConcern) } map { _ =>
-                      i + 1
-                    } recover {
-                      case _: Throwable =>
-                        logger.warn(s"Errors occurred when trying to insert record in '$collectionName' snapshot")
-                        i
-                    } getOrElse (i)
-                } match { case ssTotOk => (ok + ssTotOk, tot + cnt) }
-            } match {
-              case (inserted, count) =>
-                logger.info(s"$inserted/$count records were inserted into '$collectionName' snapshot")
-                tempDbObjects.foldLeft(0L) {
-                  case (ok, tdbo) =>
-                    val query = pidQuery(tdbo.get("_id").toString)
-                    Try { snaps.remove(query, snapsWriteConcern) } map { r =>
-                      ok + r.getN
-                    } recover {
-                      case _: Throwable =>
-                        logger.warn(s"Errors occurred when trying to remove records, previously copied to '$collectionName' snapshot, from '${settings.SnapsCollection}' snapshot")
-                        ok
-                    } getOrElse (ok)
-                } match {
-                  case removed =>
-                    logger.info(s"$removed/$count records, previously copied to '$collectionName' snapshot, were removed from '${settings.SnapsCollection}' snapshot")
-                    if (removed < inserted) (done + removed, ignored) else (done + inserted, ignored)
-                }
-            }
-          } else {
-            tempDbObjects.foldLeft(0L) {
-              case (tot, tdbo) =>
-                val query = pidQuery(tdbo.get("_id").toString)
-                val cnt = snaps.count(query).toLong
-                tot + cnt
-            } match { case ssTotIgnored => 
-                logger.info(s"$ssTotIgnored records were ignored and remain in '${settings.SnapsCollection}' journal")
-                (done, ignored + ssTotIgnored)
-            }
-          }
-        }
-      } match { 
-        case (totalOk, totalIgnored) => 
-          logger.info(s"SNAPSHOTS: $totalOk/$totalCount records were successfully transfered to suffixed collections")
-          if ( totalIgnored > 0) {
-            logger.info(s"SNAPSHOTS: $totalIgnored/$totalCount records were ignored and remain in '${settings.SnapsCollection}' snapshot")
-            if (totalIgnored + totalOk == totalCount)
-              logger.info(s"SNAPSHOTS: $totalOk + $totalIgnored = $totalCount, all records were successfully handled")
-            else
-              logger.warn(s"SNAPSHOTS: $totalOk + $totalIgnored does NOT equal $totalCount, check remaining records  in '${settings.SnapsCollection}' snapshot")
-          }
-      }
-    }
-
-    // METADATA
+    // METADATA //
 
     // Empty metadata collection, it will be rebuilt from suffixed collections through usual Akka persistence process
     val count = metadata.count()
@@ -195,12 +50,165 @@ class MigrateToSuffixedCollections(system: ActorSystem, config: Config) extends 
       case t: Throwable => logger.warn(s"Trying to empty ${settings.MetadataCollection} collection failed.", t)
     }
 
-    // CLEANING //
+    // END //
 
+    logger.info("Automatic migration to collections with suffixed names has completed")
+  }
+
+  /**
+   * Applies migration process to some category, i.e. journal or snapshot
+   */
+  private[this] def handleMigration(originCollection: MongoCollection) = {
+
+    val makeJournal: String => MongoCollection = journal
+    val makeSnaps: String => MongoCollection = snaps
+
+    // retrieve journal or snapshot properties
+    val (makeNewCollection, getNewCollectionName, writeConcern, summaryTitle) = originCollection match {
+      case c: MongoCollection if (c == journal) => (makeJournal, getJournalCollectionName(_), journalWriteConcern, "journals")
+      case c: MongoCollection if (c == snaps)   => (makeSnaps, getSnapsCollectionName(_), snapsWriteConcern, "snapshots")
+    }
+
+    // create a temporary collection
+    val temporaryCollectionName = s"migration2suffix-${System.currentTimeMillis()}-${Random.nextInt(1000)}"
+    val temporaryCollection = collection(temporaryCollectionName)
+
+    // aggregate persistenceIds, contained in unique collection, and store them in temporary collection (as "_id" field)
+    originCollection.aggregate(
+      MongoDBObject("$group" -> MongoDBObject("_id" -> s"$$$PROCESSOR_ID")) ::
+        MongoDBObject("$out" -> temporaryCollectionName) ::
+        Nil).results
+
+    // for each retrieved persistenceId, insert corresponding records from unique collection
+    // to appropriate (and newly created) suffixed collection, then remove them from unique collection
+    if (temporaryCollection.count() > 0) {
+      val totalCount = originCollection.count()
+      // we group by future suffixed collection name, foldLeft methods are only here for counting
+      temporaryCollection.find().toSeq.groupBy(tempDbObject => getNewCollectionName(tempDbObject.get("_id").toString)).foldLeft(0L, 0L) {
+        case ((done, ignored), (newCollectionName, tempDbObjects)) => {
+          // we create suffixed collection
+          val newCollection = makeNewCollection(tempDbObjects.head.get("_id").toString)
+          // we check new suffixed collection is not origin unique collection
+          if (!newCollectionName.equals(getOriginCollectionName(originCollection))) {
+            // ok, we migrate records
+            val migrated = migrateRecords(tempDbObjects, originCollection, newCollection, newCollectionName, writeConcern)
+            (done + migrated, ignored)
+          } else {
+            // no migration but we count
+            val notMigrated = ignoreRecords(tempDbObjects, originCollection)
+            (done, ignored + notMigrated)
+          }
+        }
+      } match {
+        // logging...
+        case (totalOk, totalIgnored) =>
+          logger.info(s"${summaryTitle.toUpperCase}: $totalOk/$totalCount records were successfully transfered to suffixed collections")
+          if (totalIgnored > 0) {
+            logger.info(s"${summaryTitle.toUpperCase}: $totalIgnored/$totalCount records were ignored and remain in '${getOriginCollectionName(originCollection)}'")
+            if (totalIgnored + totalOk == totalCount)
+              logger.info(s"${summaryTitle.toUpperCase}: $totalOk + $totalIgnored = $totalCount, all records were successfully handled")
+            else
+              logger.warn(s"${summaryTitle.toUpperCase}: $totalOk + $totalIgnored does NOT equal $totalCount, check remaining records  in '${getOriginCollectionName(originCollection)}'")
+          }
+      }
+    }
+
+    // remove temporary collection
     Try(temporaryCollection.drop()) recover {
       case t: Throwable => logger.warn("No temporary collection to drop", t)
     }
 
-    logger.info("Automatic migration to collections with suffixed names has completed")
   }
+
+  /**
+   * migrate records from an origin collection to some new collection and returns the total amount of migrated records
+   */
+  private[this] def migrateRecords(tempDbObjects: Seq[DBObject],
+                                   originCollection: MongoCollection,
+                                   newCollection: MongoCollection,
+                                   newCollectionName: String,
+                                   writeConcern: WriteConcern): Long = {
+    // first step: we insert records in new suffixed collection, foldLeft methods are only here for counting
+    tempDbObjects.foldLeft(0L, 0L) {
+      case ((ok, tot), tdbo) =>
+        val query = pidQuery(tdbo)
+        val cnt = originCollection.count(query).toLong
+        originCollection.find(query).foldLeft(0L) {
+          insertIntoCollection(newCollection, newCollectionName, writeConcern)
+        } match { case ssTotOk => (ok + ssTotOk, tot + cnt) }
+    } match {
+      case (inserted, count) =>
+        logger.info(s"$inserted/$count records were inserted into '$newCollectionName'")
+        // 2nd step: we remove records from unique collection
+        tempDbObjects.foldLeft(0L) {
+          removeFromCollection(originCollection, writeConcern)
+        } match {
+          case removed =>
+            logger.info(s"$removed/$count records, previously copied to '$newCollectionName', were removed from '${getOriginCollectionName(originCollection)}'")
+            if (removed < inserted) removed else inserted
+        }
+    }
+  }
+
+  /**
+   * insert one record into some collection and returns the total amount of inserted records
+   */
+  private[this] def insertIntoCollection(newCollection: MongoCollection,
+                                         newCollectionName: String,
+                                         writeConcern: WriteConcern): (Long, DBObject) => Long = { (i, dbo) =>
+    Try { newCollection.insert(dbo, writeConcern) } map { _ =>
+      i + 1
+    } recover {
+      case _: Throwable =>
+        logger.warn(s"Errors occurred when trying to insert record in '$newCollectionName'")
+        i
+    } getOrElse (i)
+  }
+
+  /**
+   * remove records from some collection and returns the total amount of removed records
+   */
+  private[this] def removeFromCollection(originCollection: MongoCollection,
+                                         writeConcern: WriteConcern): (Long, DBObject) => Long = { (i, dbo) =>
+    Try { originCollection.remove(pidQuery(dbo), writeConcern) } map { r =>
+      i + r.getN
+    } recover {
+      case _: Throwable =>
+        logger.warn(s"Errors occurred when trying to remove records from '${getOriginCollectionName(originCollection)}'")
+        i
+    } getOrElse (i)
+
+  }
+
+  /**
+   * count records ignored by migration process
+   */
+  private[this] def ignoreRecords(tempDbObjects: Seq[DBObject],
+                                  originCollection: MongoCollection): Long = {
+    tempDbObjects.foldLeft(0L) {
+      case (tot, tdbo) =>
+        val query = pidQuery(tdbo)
+        val cnt = originCollection.count(query).toLong
+        tot + cnt
+    } match {
+      case ignored =>
+        logger.info(s"$ignored records were ignored and remain in '${getOriginCollectionName(originCollection)}'")
+        ignored
+    }
+
+  }
+
+  /**
+   * Convenient method to generate a simple query widely used in this class
+   */
+  private[this] def pidQuery(dbo: DBObject) = (PROCESSOR_ID $eq dbo.get("_id").toString)
+
+  /**
+   * Convenient method to retrieve origin collection name
+   */
+  private[this] def getOriginCollectionName(originCollection: MongoCollection): String = originCollection match {
+    case c: MongoCollection if (c == journal) => settings.JournalCollection
+    case c: MongoCollection if (c == snaps)   => settings.SnapsCollection
+  }
+
 }
