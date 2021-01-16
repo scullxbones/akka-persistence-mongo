394a84e1ae542cda6fc66e1324f16d2e25296810
Brian Scully <scullduggery@gmail.com>
2021-01-16 08:58:35 -0500

    Prepare for v3.0.6 release

--- release_process_pre.sh
+++ release_process_pre.sh
@@ -5,10 +5,6 @@ die () {
     exit 1
 }
 
-if [ "$GH_TOKEN" == "" ]; then
-    die "Missing github oauth token"
-fi
-
 NEXT=$1
 NEXT_WO_V=$(echo $1 | sed -Ee 's/^v([.0-9]+)$/\1/')
 PREVIOUS=$(git describe --abbrev=0 --tags)
@@ -25,34 +21,19 @@ sed -i '' -e "s/$PREVIOUS_WO_V/$NEXT_WO_V/" README.md
 sed -i '' -e "s/$PREVIOUS_WO_V/$NEXT_WO_V/" docs/akka26.md
 sed -i '' -e "s/^val releaseV = \"$PREVIOUS_WO_V\"$/val releaseV = \"$NEXT_WO_V\"/" build.sbt
 
-read -r -d '' BLOCK <<SECTION
+cat <<SECTION >target/release.md
+
 ### $NEXT_WO_V
 `git log $(git describe --tags --abbrev=0)..HEAD --oneline | cut -d' ' -f 2- | sed -e 's/^/* /' `
-
 SECTION
 
-perl -ni -e "print; print \"\n$BLOCK\n\" if $. == 1" docs/changelog26.md
+cp docs/changelog26.md previous.md
+awk '//; /^## Changelog/{while(getline<"target/release.md"){print}}' previous.md
+mv previous.md docs/changelog26.md
 
 git add .
 git commit -m 'Prepare for '$NEXT' release' -S
 git tag -a $NEXT -m "$BLOCK" -s
 
-CR=$(printf '\r')
-BLOCK_WITH_CR=$(echo -e "$BLOCK" | sed -e "s/\$/$CR/g")
-
-cat <<API_JSON >api.json 
-{
-    "tag_name": "$NEXT",
-    "target_commitish": "master",
-    "name": "$NEXT",
-    "draft": true,
-    "body": "$BLOCK"
-}
-API_JSON
-
-curl -v -H "Content-Type: application/json" -XPOST \
-    -d @api.json \
-    -H 'Authorization: token $GH_TOKEN'  \
-    https://api.github.com/repos/scullxbones/akka-persistence-mongo/releases
-
-rm api.json
+gh release create $NEXT -F target/release.md -d -t $NEXT --target master
+rm target/release.md
